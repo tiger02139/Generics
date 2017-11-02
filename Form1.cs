938a5f24501cc22a2f78b5be4eb79dcca5aa0f60
skim_brk <skim@cambridgeassociates.com>
2017-11-02 14:20:14 -0400

    added Dictionary<,> example

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Generics
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenericStaticClass.GreenEggsAndHam();
+        }
     }
 }
---
+++ GenericStaticClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public static class GenericStaticClass
+    {
+        public static void GreenEggsAndHam()
+        {
+            string text = @"Do you like green eggs and ham?
+                            I do not like them, Sam-I-am.
+                            I do not like green eggs and ham.";
+
+            Dictionary<string, int> frequencies = CountWords(text);
+            foreach (KeyValuePair<string, int> entry in frequencies)
+            {
+                string word = entry.Key;
+                int frequency = entry.Value;
+                //Console.WriteLine($"{word}: {frequency}");
+                Console.WriteLine("{0}:{1}", word, frequency);
+            }
+        }
+
+        private static Dictionary<string, int> CountWords(string text)
+        {
+            Dictionary<string, int> frequencies;
+            frequencies = new Dictionary<string, int>();
+
+            string[] words = Regex.Split(text, @"\W+");
+
+            foreach (string word in words)
+            {
+                if (frequencies.ContainsKey(word))
+                {
+                    frequencies[word]++;
+                }
+                else
+                {
+                    frequencies[word] = 1;
+                }
+            }
+
+            return frequencies;
+        }
+    }
+}
