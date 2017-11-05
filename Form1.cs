fdc8412028a72da465189414a5c8943f98173f0c
tiger02139 <sander414@gmail.com>
2017-11-04 23:15:55 -0400

    added non-generic class for comparison

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Generics
@@ -20,6 +13,11 @@ namespace Generics
         private void button1_Click(object sender, EventArgs e)
         {
             GenericStaticClass.GreenEggsAndHam();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NonGenericStaticClass.GreenEggsAndHam();
         }
     }
 }
---
+++ NonGenericStaticClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Generics
+{
+    /// <summary>
+    /// This is the C#1 analog of GenericStaticClass.
+    /// Notice that I had to cast the key and value because
+    /// they are intrinsically objects.
+    /// 1. Execution-time type checking as opposed to compile-time type checking.
+    /// 2. All of the casts make code harder to read and understand
+    /// 3. Performance hit because of type-checking and boxing/unboxing.
+    /// </summary>
+    public static class NonGenericStaticClass
+    {
+        public static void GreenEggsAndHam()
+        {
+            string text = @"Do you like green eggs and ham?
+                            I do not like them, Sam-I-am.
+                            I do not like green eggs and ham.";
+
+            Hashtable frequencies = CountWords(text);
+            foreach (DictionaryEntry entry in frequencies)
+            {
+                string word = (string)entry.Key;
+                int frequency = (int)entry.Value;  //unboxing int
+                Console.WriteLine("{0}:{1}", word, frequency);
+            }
+        }
+
+        private static Hashtable CountWords(string text)
+        {
+            Hashtable frequency = new Hashtable();
+
+            string[] words = Regex.Split(text, @"\W+");
+
+            foreach (string word in words)
+            {
+                if (frequency.ContainsKey(word))
+                {
+                    frequency[word] = (int)frequency[word] + 1;  //boxing int
+                }
+                else
+                {
+                    frequency[word] = 1; //boxing int
+                }
+            }
+
+            return frequency;
+        }
+    }
+}
